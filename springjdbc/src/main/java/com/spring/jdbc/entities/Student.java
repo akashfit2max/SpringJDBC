cdd95a71daea3a1081fde8878f7b49c608d2daad
Akash kumar Sahoo <akashnikz@gmail.com>
2021-09-26 12:02:28 +0530

    new

--- springjdbc/src/main/java/com/spring/jdbc/entities/Student.java
+++ springjdbc/src/main/java/com/spring/jdbc/entities/Student.java
@@ -1,49 +1,49 @@
 package com.spring.jdbc.entities;
 
 public class Student {
-	private int id;
-	private String name;
-	private String city;
+    private int id;
+    private String name;
+    private String city;
 
-	public Student() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    public Student() {
+	super();
+	// TODO Auto-generated constructor stub
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+	return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+	this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+	return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+	this.name = name;
+    }
 
-	public String getCity() {
-		return city;
-	}
+    public String getCity() {
+	return city;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setCity(String city) {
+	this.city = city;
+    }
 
-	public Student(int id, String name, String city) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.city = city;
-	}
+    public Student(int id, String name, String city) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.city = city;
+    }
 
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", city=" + city + "]";
-	}
+    @Override
+    public String toString() {
+	return "Student [id=" + id + ", name=" + name + ", city=" + city + "]";
+    }
 
 }
